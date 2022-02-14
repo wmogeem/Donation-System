673165a4a2bb28a2af64f63e3c35771108deb120
Faris Khiamy <farisfareed.k@gmail.com>
2022-02-14 10:41:59 +0300

    Sign up input validation

--- app/src/main/java/io/github/ifariskh/donationsystem/activity/SignUpActivity.java
+++ app/src/main/java/io/github/ifariskh/donationsystem/activity/SignUpActivity.java
@@ -13,11 +13,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.Calendar;
+import java.util.regex.Pattern;
 
 import io.github.ifariskh.donationsystem.R;
 import io.github.ifariskh.donationsystem.core.User;
 
-public class SignUpActivity extends AppCompatActivity implements View.OnClickListener{
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextInputLayout fullName, email, id, password, phone;
     private EditText dob;
@@ -45,6 +46,7 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 
     private void initCalender() {
         final Calendar CALENDER = Calendar.getInstance();
+        CALENDER.add(Calendar.YEAR, -18);
         final int YEAR = CALENDER.get(Calendar.YEAR);
         final int MONTH = CALENDER.get(Calendar.MONTH);
         final int DAY = CALENDER.get(Calendar.DAY_OF_MONTH);
@@ -58,31 +60,121 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
                             @Override
                             public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
                                 i1 = i1 + 1;
-                                String date = i2 + "/" + i1 +"/" + i;
+                                String date = i2 + "/" + i1 + "/" + i;
                                 dob.setText(date);
                             }
                         }, YEAR, MONTH, DAY);
+                datePickerDialog.getDatePicker().setMaxDate(CALENDER.getTime().getTime());
                 datePickerDialog.show();
             }
         });
     }
 
+    private boolean validateFullName() {
+        String val = fullName.getEditText().getText().toString().trim();
+        fullName.setErrorEnabled(true);
+        if (val.isEmpty()) {
+            fullName.setError("*Required");
+            return false;
+        } else if (!Pattern.compile("^[ A-Za-z]{4,}+$").matcher(val).matches()) {
+            fullName.setError("*Name should only have letters and space and at least 4 character");
+            return false;
+        } else {
+            fullName.setError(null);
+            fullName.setErrorEnabled(false);
+        }
+        return true;
+    }
+
+    private boolean validateEmail() {
+        String val = email.getEditText().getText().toString().trim();
+        email.setErrorEnabled(true);
+        if (val.isEmpty()) {
+            email.setError("*Required");
+            return false;
+        } else if (!Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE).matcher(val).matches()) {
+            email.setError("*Invalid email address");
+            return false;
+        } else {
+            email.setError(null);
+            email.setErrorEnabled(false);
+        }
+        return true;
+    }
+
+    private boolean validateId() {
+        String val = id.getEditText().getText().toString().trim();
+        id.setErrorEnabled(true);
+        if (val.isEmpty()) {
+            id.setError("*Required");
+            return false;
+        } else if (!Pattern.compile("^[0-9]{10}+$").matcher(val).matches()) {
+            id.setError("*Invalid ID");
+            return false;
+        } else {
+            id.setError(null);
+            id.setErrorEnabled(false);
+        }
+        return true;
+    }
+
+    private boolean validatePassword() {
+        String val = password.getEditText().getText().toString().trim();
+        password.setErrorEnabled(true);
+        if (val.isEmpty()) {
+            password.setError("*Required");
+            return false;
+        } else if (!Pattern.compile(
+                "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,16}$")
+                .matcher(val).matches()) {
+            password.setError("Password must contain at least one digit [0-9].\n" +
+                    "Password must contain at least one lowercase Latin character [a-z].\n" +
+                    "Password must contain at least one uppercase Latin character [A-Z].\n" +
+                    "Password must contain at least one special character like ! @ # & ( ).\n" +
+                    "Password must contain a length of at least 8 characters and a maximum of 20 characters.");
+            return false;
+        } else {
+            password.setError(null);
+            password.setErrorEnabled(false);
+        }
+        return true;
+    }
+
+    private boolean validatePhone() {
+        String val = phone.getEditText().getText().toString().trim();
+        phone.setErrorEnabled(true);
+        if (val.isEmpty()) {
+            phone.setError("*Required");
+            return false;
+        } else if (!Pattern.compile("^[0-9]{9}+$").matcher(val).matches()) {
+            phone.setError("*Invalid phone number should be 10 numbers");
+            return false;
+        } else {
+            phone.setError(null);
+            phone.setErrorEnabled(false);
+        }
+        return true;
+    }
+
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.sign_in_button:
                 Intent intent = new Intent(this, SignInActivity.class);
                 startActivity(intent);
                 break;
             case R.id.sign_up_button:
-                User user = new User(
-                        fullName.getEditText().getText().toString().trim(),
-                        email.getEditText().getText().toString().trim(),
-                        id.getEditText().getText().toString().trim(),
-                        phone.getEditText().getText().toString().trim(),
-                        dob.getText().toString().trim()
-                );
-                user.register(password.getEditText().getText().toString().trim(), this);
+                if (validateFullName() && validateEmail() && validateId() && validatePassword() && validatePhone()) {
+                    User user = new User(
+                            fullName.getEditText().getText().toString().trim(),
+                            email.getEditText().getText().toString().trim(),
+                            id.getEditText().getText().toString().trim(),
+                            phone.getEditText().getText().toString().trim(),
+                            dob.getText().toString().trim()
+                    );
+                    user.register(password.getEditText().getText().toString().trim(), this);
+                }
+
                 break;
         }
     }
